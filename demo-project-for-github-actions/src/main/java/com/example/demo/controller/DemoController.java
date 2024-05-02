5ed55326373f6f463256583def2c83b8520402a9
shreyashHake <shreyashhake4@gmail.com>
2024-05-02 17:18:37 +0530

    Initial commit

---
+++ demo-project-for-github-actions/src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class DemoController {
+
+    @GetMapping("/greet")
+    public String sayHello() {
+        return "hello, have a nice date.";
+    }
+}
